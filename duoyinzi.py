dd5c674a99cc1af1ef93b3d4336bf06c1e4cb9d3
Ender <thelastender@gmail.com>
2022-01-07 23:21:57 +0800

    duoyinzi worked
    Change-Id: Iadcb82438c36d09a359bab7fc8c12d7a8b9ab183

--- duoyinzi.py
+++ duoyinzi.py
@@ -34,55 +34,83 @@ def gen(title, s, output, with_result=False):
     width, height = A4 # A4纸大小
     from reportlab.lib.units import mm
     c = Canvas(output + '.pdf', initialFontName='song', bottomup=0)
+    font_factor = 0.35146
+    title_font_size = 20
+    content_font_size = 14
+    footer_font_size = 10
     top = 20 * mm # 上边距20mm
+    pinyin_step = 10 * mm
+    character_step = font_factor * content_font_size * mm
+    word_height = 15 * mm
+    line_padding = 5 * mm
+    margin_top = 30 * mm
+    margin_left = 15 * mm
+    padding = 4 * mm
+    x = margin_left
+    y = margin_top
+    line_step = 0
     page = 1
     # title
     c.setFontSize(20)
-    c.drawString(width / 2 - 15 * mm, top, '%s %d' % (outputname, page))
+    c.drawString(width / 2 - len(outputname) * character_step / 2, top, '%s' % outputname)
     # content
-    def draw_block():
-        pass
-    c.setFontSize(14)
     i = 0
-    word_step = 15 * mm
-    character_step = 15 * mm
-    line_step = 25 * mm
-    margin_top = 30 * mm
-    margin_left = 15 * mm
-    padding = 2 * mm
-    x = margin_left
-    y = margin_top
-    while i < len(s):
-        if x + len(s[i]) * character_step > width - margin_left:
-            y = y + line_step
+    c.setFontSize(content_font_size)
+    for k, v in s.items():
+        w = character_step + padding + pinyin_step + (5 * character_step if not with_result else max(list(map(lambda x: len(x), v.values()))) * character_step + 3 * character_step)
+        # 判断是否需要换行
+        if x + w > width - margin_left and x > margin_left:
             x = margin_left
+            y = y + line_step + line_padding
+            line_step = 0
+            # 判断是否超过一页
             if y > height - margin_top:
                 y = margin_top
+                # footer
+                c.setFontSize(footer_font_size)
+                c.drawString(width / 2 - len(str(page)) * character_step / 2, height - top / 2, '%d' % page)
                 c.showPage()
                 page = page + 1
                 # title
-                c.setFontSize(20)
-                c.drawString(width / 2 - 15 * mm, top, '%s %d' % (outputname, page))
-                c.setFontSize(14)
+                c.setFontSize(title_font_size)
+                c.drawString(width / 2 - len(outputname) * character_step / 2, top, '%s' % outputname)
+                c.setFontSize(content_font_size)
+        h = word_height * len(v)
+        line_step = max(line_step, h)
+        c.drawString(x, y + (h - word_height) / 2 , k)
+        # c.rect(x, y, w, h)
+        # 大括号
+        c.line(x + character_step + padding / 4, y + (h - word_height - character_step * 2/3) / 2, 
+                    x + character_step + padding * 2 / 4, y + (h - word_height - character_step * 2/3) / 2)
+        c.line(x + character_step + padding * 2 / 4, y + (- character_step * 2/3) / 2, 
+                    x + character_step + padding * 3 / 4, y + (- character_step * 2/3) / 2)
+        c.line(x + character_step + padding * 2 / 4, y + h - word_height - (character_step * 2/3) / 2, 
+                    x + character_step + padding * 3 / 4, y + h - word_height - (character_step * 2/3) / 2)
+        c.line(x + character_step + padding * 2 / 4, y - (character_step * 2/3) / 2, 
+                    x + character_step + padding * 2 / 4, y + h - word_height - (character_step * 2/3) / 2)
         j = 0
-        while j < len(s[i]):
-            k = s[i][j]
-            c.drawString(x + padding, y, k)
-            c.rect(x, y + padding, character_step, character_step)
-            # print(k, x / mm)
-            x = x + character_step
+        for p, word in v.items():
+            c.drawString(x + character_step + padding, y + j * word_height, p)
+            if with_result:
+                c.drawString(x + character_step + padding + pinyin_step, y + j * word_height, '（%s）' % word)
+            else:
+                c.drawString(x + character_step + padding + pinyin_step, y + j * word_height, '（')
+                c.drawString(x + character_step + padding + pinyin_step + 4 * character_step, y + j * word_height, '）')
             j = j + 1
-        if j > 0:
-            x = x + character_step
-        # x = x + word_step
+        x = x + w
         i = i + 1
+    # footer
+    c.setFontSize(footer_font_size)
+    c.drawString(width / 2 - len(str(page)) * character_step / 2, height - top / 2, '%d' % page)
     c.save()
 
 if __name__ == '__main__':
     inputname = sys.argv[1]
     outputname = sys.argv[2]
+    with_result = False
+    if len(sys.argv) > 3:
+        with_result = bool(int(sys.argv[3]))
 
     init()
     s = process(inputname)
-    print(s)
-    gen(outputname, s, outputname)
+    gen(outputname, s, outputname, with_result)
